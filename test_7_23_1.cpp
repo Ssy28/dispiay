5a0c548e7e93f5263e663a8e432e42e32d144612
沈圣杨 <15826742233@163.com>
2023-07-23 22:05:50 +0800

    Add files via upload

---
+++ test_7_23_1.cpp
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+// int Fac1(int x)
+// {
+//     int i = 1;
+//     int ret = 1;
+//     for(i=1; i<=x; i++)
+//         ret *= i;
+//     return ret;
+// }
+int Fac2(int x)
+{
+    if(x<=1)
+        return 1;
+    else
+        return x*Fac2(x-1);
+}
+
+int main()
+{
+    int n = 0;
+    int ret = 0;
+    scanf("%d", &n);
+    if(n>=0)
+    {
+        ret = Fac2(n);
+        printf("%d\n", ret);
+    }
+    else//无法判断小数
+        printf("输入错误\n");
+    return 0;
+}
