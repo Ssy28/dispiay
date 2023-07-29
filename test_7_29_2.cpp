28b1bda14e33e56eec26e44264b02dc2da801630
Ssy28 <15826742233@163.com>
2023-07-29 21:34:51 +0800

    test

---
+++ test_7_29_2.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int num = 0;
+    int count = 0;
+    scanf("%d", &num);
+
+    int i = 0;
+    for(i=0; i<32; i++)
+    {
+        if(1 == ((num >> i) & 1))
+            count++;
+    }
+
+    printf("%d\n", count);
+    return 0;
+}
