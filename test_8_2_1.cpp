0f5ca9add3c216d628320afd71e1078eb7857f60
Ssy28 <15826742233@163.com>
2023-08-02 21:25:45 +0800

    test

---
+++ test_8_2_1.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+struct Stu
+{
+    //成员变量
+    char name[20];
+    int age;
+    char id[20];
+};
+
+int main()
+{
+    //使用struct Stu这个类型创造了一个学生对象s1,并初始化
+    struct Stu s1 = {"张三", 20, "204310119"};
+    struct Stu *ps = &s1;
+    printf("%s\n", (*ps).name);
+    printf("%d\n", (*ps).age);
+    printf("%s\n", (*ps).id);
+    return 0;
+}
