ea4c1c47a64683f9a2533e32f3c4c19125c3d897
Ssy28 <15826742233@163.com>
2023-08-04 21:18:21 +0800

    test

--- test_8_2_1.cpp
+++ test_8_2_1.cpp
@@ -13,8 +13,12 @@ int main()
     //使用struct Stu这个类型创造了一个学生对象s1,并初始化
     struct Stu s1 = {"张三", 20, "204310119"};
     struct Stu *ps = &s1;
-    printf("%s\n", (*ps).name);
-    printf("%d\n", (*ps).age);
-    printf("%s\n", (*ps).id);
+    // printf("%s\n", (*ps).name);
+    // printf("%d\n", (*ps).age);
+    // printf("%s\n", (*ps).id);
+    printf("%s\n", ps->name);
+    printf("%d\n", ps->age);
+    printf("%s\n", ps->id);
+    // 结构体指针->成员名
     return 0;
 }
---
+++ test_8_4_1.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    char a = 3;
+    char b = 127;
+    char c = a + b;
+    printf("%d", c);
+    return 0;
+}
