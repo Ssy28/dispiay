800113583cde44aec8fc09d3673a42292a10769b
Ssy28 <15826742233@163.com>
2023-08-05 08:53:16 +0800

    test

--- test_8_4_1.cpp
+++ test_8_4_1.cpp
@@ -4,7 +4,7 @@ int main()
 {
     char a = 3;
     char b = 127;
-    char c = a + b;
+    char c = a + b;//整形提升，截断
     printf("%d", c);
     return 0;
 }
---
+++ test_8_5_1.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    char a = 0xb6;
+    char b = 0x36;
+    if(a == 0xb6)
+        printf("a");
+    if(a == 0x36)
+        printf("b");
+    return 0;
+}
