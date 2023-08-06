79fa3847e081711d3dd95b25f359aa2d4e023085
Ssy28 <15826742233@163.com>
2023-08-06 11:19:39 +0800

    test

---
+++ test_8_6_1.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int my_strlen(char* str)
+{
+    char* start = str;
+    char* end = str;
+    while (*end != '\0')
+    {
+        end++;
+    }
+    return end - start;
+}
+
+int main()
+{
+    char arr[] = "bit";
+    int len = my_strlen(arr);
+    printf("%d", len);
+    return 0;
+}
