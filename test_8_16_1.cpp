4d388f9e54b05a190d48748fcfb7252be82651c4
Ssy28 <15826742233@163.com>
2023-08-16 14:52:56 +0800

    test

---
+++ test_8_16_1.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char a[1000];
+    int i;
+
+    for(i=0; i<1000; i++)
+    {
+        a[i] = -1 - i;
+    }
+    
+    printf("%d\n", strlen(a));
+    return 0;
+}
