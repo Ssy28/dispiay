fa72ac9d0027b8cabed3adb41b5860d88fc84c0b
Ssy28 <15826742233@163.com>
2023-08-18 14:50:55 +0800

    test

--- test_8_17_1.cpp
+++ test_8_17_1.cpp
@@ -4,13 +4,13 @@ int main()
 {
     int n = 9;
     float *pFloat = (float *)&n;
-    printf("n的值为: %d\n", n);
-    printf("*pFloat的值为: %f\n", *pFloat);
+    printf("n的值为: %d\n", n);//9
+    printf("*pFloat的值为: %f\n", *pFloat);//0.000000
     //整形与浮点数的存储方式不同
     //浮点数的存储：(-1)^s * m * 2^e ; 存储S、M、E
     //9.0: (-1)^0 * 1.001 * 2^3
     *pFloat = 9.0;
-    printf("num的值为: %d\n", n);
-    printf("*pFloat的值为: %f\n", *pFloat);
+    printf("num的值为: %d\n", n);//1091567616
+    printf("*pFloat的值为: %f\n", *pFloat);//9.000000
     return 0;
 }
